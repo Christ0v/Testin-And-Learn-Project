268fa843ef70869bdb4390b4df18a9c813845f0f
Christ0v <krzyspajak7@gmail.com>
2022-11-03 16:54:52 +0100

    New/ old project

---
+++ Fizyka/Fizyka/Assets/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float force = 5.0f;
+
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(Vector3.right * force);
+        };
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(Vector3.left * force);
+        };
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(Vector3.forward * force);
+        };
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(Vector3.back * force);
+        };
+    }
+}
